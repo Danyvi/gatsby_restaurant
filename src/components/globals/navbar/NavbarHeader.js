4df81c4935a33b96f7989838fffbe5aa6c6bd8ba
Daniele Vinci <vinci.dani@gmail.com>
2019-08-02 17:26:47 -0400

    Navbar Header Component

--- src/components/globals/navbar/NavbarHeader.js
+++ src/components/globals/navbar/NavbarHeader.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
-
+import {Link} from 'gatsby';
+import logo from '../../../images/logo.svg'
+// import {FaAlignRight} from 'react-icons/fa'
+import {FaBars} from 'react-icons/fa'
+import styled from 'styled-components'
 export default class NavbarHeader extends Component {
   render() {
+    const {handleNavbar} = this.props
     return (
-      <div>
-        content from NavbarHeader
-      </div>
+      <HeaderWrapper>
+        <Link to="/">
+          <img src={logo} alt="company name" />
+        </Link>
+        <FaBars className="toggle-icon" onClick={()=>{handleNavbar()}}></FaBars>
+      </HeaderWrapper>
     )
   }
 }
+
+const HeaderWrapper = styled.div`
+padding:0.4rem 1rem;
+display: flex;
+align-items: center;
+justify-content: space-between;
+
+.toggle-icon {
+  font-size: 1.75rem;
+  color: yellow;
+  cursor: pointer;
+}
+
+@media (min-width: 768px) {
+  .toggle-icon {
+    display: none;
+  }
+}
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 // import { Link } from "gatsby"
 // import styled from "styled-components";
-import { FaBeer } from 'react-icons/fa';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
