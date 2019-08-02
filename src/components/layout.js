79d247b8c6a12dac989d9d66bb579ae5fb8baf3a
Daniele Vinci <vinci.dani@gmail.com>
2019-08-02 16:54:31 -0400

    Navbar Component State and Methods

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import {createGlobalStyle} from "styled-components";
+import Navbar from "../components/globals/navbar"
 // import "./bootstrap.min.css"
 // import "./layout.css"
 // import "../sass/layout.scss"
@@ -18,6 +19,7 @@ const Layout = ({children})=>{
   return (
     <React.Fragment>
     <GlobalStyle />
+    <Navbar />
       {children}
     </React.Fragment>
   )
