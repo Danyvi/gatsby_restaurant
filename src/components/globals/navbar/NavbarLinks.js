4e79decfe6a27723473a0a59fb1809dc7fe62def
Daniele Vinci <vinci.dani@gmail.com>
2019-08-03 12:33:58 -0400

    Navbar Links Screen Size

--- src/components/globals/navbar/NavbarLinks.js
+++ src/components/globals/navbar/NavbarLinks.js
@@ -35,41 +35,54 @@ export default class NavbarLinks extends Component {
     
     return (
       <LinkWrapper open={this.props.navbarOpen}>
-        {this.state.links.map(item=>{
-          return (
-            <li key={item.id}>
-              <Link to={item.path} className="nav-link">
-                {item.name}
-              </Link>
-            </li>
-          )
-        })}
+        {
+          this.state.links.map(item=>{
+            return (
+              <li key={item.id}>
+                <Link to={item.path} className="nav-link">
+                  {item.name}
+                </Link>
+              </li>
+            )
+          })
+        }
       </LinkWrapper>
     )
   }
 }
 
 const LinkWrapper = styled.ul`
-li{
-  list-style-type: none;
-}
-
-.nav-link{
-  display: block;
-  text-decoration: none;
-  padding: 0.5rem 1rem 0.5rem 1rem;
-  color: ${styles.colors.mainGrey};
-  font-weight: 700;
-  text-transform: capitalize;
-  cursor: pointer;
-  ${styles.transDefault};
-  &:hover {
-    background: ${styles.colors.mainGrey};
-    color: ${styles.colors.mainYellow};
-    padding: 0.5rem 1rem 0.5rem 1.3rem;
+  li{
+    list-style-type: none;
   }
-}
 
-height: ${props => (props.open ? '152px' : '0px')};
-overflow:hidden;
+  .nav-link{
+    display: block;
+    text-decoration: none;
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    color: ${styles.colors.mainGrey};
+    font-weight: 700;
+    text-transform: capitalize;
+    cursor: pointer;
+    ${styles.transDefault};
+    &:hover {
+      background: ${styles.colors.mainGrey};
+      color: ${styles.colors.mainYellow};
+      padding: 0.5rem 1rem 0.5rem 1.3rem;
+    }
+  }
+  height: ${props => (props.open ? '152px' : '0px')};
+  overflow: hidden;
+
+  ${styles.transObject({})};
+
+  @media (min-width: 768px) {
+    height: auto;
+    display: flex;
+    margin: 0 auto;
+    .nav-link:hover {
+      background: ${styles.colors.mainWhite};
+      padding: 0.5rem 1rem 0.5rem 1rem;
+    }
+  }
 `
