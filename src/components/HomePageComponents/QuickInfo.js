f8d99a29864e472032813e79a90d29b512591b5f
Daniele Vinci <vinci.dani@gmail.com>
2019-08-04 21:45:04 -0400

    Section Title Component

--- src/components/HomePageComponents/QuickInfo.js
+++ src/components/HomePageComponents/QuickInfo.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-
+import { Section, Title } from '../../utils'
 export default class QuickInfo extends Component {
   render() {
     return (
-      <div>
-        Content of Quick Info 
-      </div>
+      <Section>
+        <Title message="let us tell you" title="our mission" />
+      </Section>
     )
   }
 }
---
+++ src/utils/Section.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Section = styled.section`
+  padding: 2rem 0rem;
+  width: 90vw;
+  margin: 0 auto;
+`
+
+// section formatting that can be used anywhere in the project
